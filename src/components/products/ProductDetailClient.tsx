7acca2b813f96da79786be5c5bcb4fe0213ade2b
eerdem5387 <102296047+eerdem5387@users.noreply.github.com>
2025-12-03 12:19:01 +0300

    lkjlk

--- src/components/products/ProductDetailClient.tsx
+++ src/components/products/ProductDetailClient.tsx
@@ -192,7 +192,7 @@ export default function ProductDetailClient({ product }: ProductDetailClientProp
   }, [])
 
   return (
-    <div>
+    <>
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         {/* Product Images */}
@@ -523,6 +523,6 @@ export default function ProductDetailClient({ product }: ProductDetailClientProp
       </div>
     </div>
     <Toast toasts={toasts} onRemove={removeToast} />
-    </div>
+    </>
   )
 } 
